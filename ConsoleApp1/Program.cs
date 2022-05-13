e9591b40308746ca20f23002628bb667abf8bd49
Francisco Salvador Diaz Ortiz <N00244919@upn.pe>
2022-05-12 22:56:47 -0500

    Eh implementado la primera parte , lo que falta te lo dejo a ti Luis.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Declarar las variables
+            int num1;
+            int num2;
+            int resSuma;
+            int resResta;
+            int resMulti;
+            int resDiv;
+            //2.Pedir datos del usuario
+            Console.WriteLine("ingresa el primer numero: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingresa el segundo numero: ");
+            num2=Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
